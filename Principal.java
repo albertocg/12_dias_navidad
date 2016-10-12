0b47f417d081d43f480b21ac236e93272080426e
alberto castillo gordón <acastillogordon@gmail.com>
2016-10-12 11:29:20 -0500

    Commit inicial

---
+++ Principal.java
@@ -0,0 +1,84 @@
+import javax.swing.*;
+import java.io.*;
+
+class Cancion {
+    private String[] letra;
+    private ImageIcon[] imagen;
+
+    Cancion(){
+        letra = new String[12];
+        imagen = new ImageIcon[12];
+        letra[0] = "El primer día de navidad mi amor me regaló: una perdiz en un peral";
+        letra[1] = "El segundo día de navidad mi amor me regaló: dos tortolas";
+        letra[2] = "El tercer día de navidad mi amor me regaló: tres gallinas francesas";
+        letra[3] = "El cuarto día de navidad mi amor me regaló: cuatro pájaros llamando";
+        letra[4] = "El quinto día de navidad mi amor me regaló: cinco anillos de oro";
+        letra[5] = "El sexto día de navidad mi amor me regaló: seis ocas poniendo";
+        letra[6] = "El séptimo día de navidad mi amor me regaló: siete cisnes nadando";
+        letra[7] = "El octavo día de navidad mi amor me regaló: ocho criadas ordenando";
+        letra[8] = "El noveno día de navidad mi amor me regaló: nueve señoras bailando";
+        letra[9] ="El décimo día de navidad mi amor me regaló: diez señores brincando";
+        letra[10] = "El undécimo día de navidad mi amor me regaló: once gaiteros tocando";
+        letra[11] ="El duodécimo día de navidad mi amor me regaló: doce tamborileros tamboreando";
+
+        imagen[0] = new ImageIcon("image1.png");
+        imagen[1] = new ImageIcon("image2.png");
+        imagen[2] = new ImageIcon("image3.png");
+        imagen[3] = new ImageIcon("image4.png");
+        imagen[4] = new ImageIcon("image5.png");
+        imagen[5] = new ImageIcon("image6.png");
+        imagen[6] = new ImageIcon("image7.png");
+        imagen[7] = new ImageIcon("image8.png");
+        imagen[8] = new ImageIcon("image9.png");
+        imagen[9] = new ImageIcon("image10.png");
+        imagen[10] = new ImageIcon("image11.png");
+        imagen[11] = new ImageIcon("image12.png");
+    }
+
+    public String getLetra(int dia){
+        return letra[dia-1];
+    }
+
+    public ImageIcon getImagen(int dia){
+        return imagen[dia-1];
+    }
+}
+
+public class Principal {
+    public static void main(String[] args) {
+        int opc;
+        String continuar;
+        JFrame frame = new JFrame("Proyecto 1");
+        Cancion obj_cancion = new Cancion();
+
+        JOptionPane.showMessageDialog(frame, "Los 12 días de navidad");
+        do{
+            opc = 0;
+            try{
+                opc = Integer.parseInt(JOptionPane.showInputDialog(frame, "Introduzca un dia[1-12]."));
+            }catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(frame, "Error de formato: " + e.getMessage(), "Excepcion", JOptionPane.WARNING_MESSAGE);
+            }catch(Exception e){
+                JOptionPane.showMessageDialog(frame, "Error: " + e.getMessage(), "Excepcion", JOptionPane.WARNING_MESSAGE);
+            }
+
+            for(int i=opc; i>0; i--){
+                try{
+                    JOptionPane.showMessageDialog(frame, obj_cancion.getLetra(i), "Cancion", JOptionPane.INFORMATION_MESSAGE, obj_cancion.getImagen(i)); 
+                }catch(ArrayIndexOutOfBoundsException e){
+                    JOptionPane.showMessageDialog(frame, "Valor fuera de rango: " + e.getMessage(), "Excepcion", JOptionPane.WARNING_MESSAGE);
+                }catch(Exception e){
+                    JOptionPane.showMessageDialog(frame, "Error: " + e.getMessage(), "Excepcion", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+
+            try{
+                continuar = JOptionPane.showInputDialog(frame, "Escriba 'si' para continuar");
+            }catch(Exception e){
+                JOptionPane.showMessageDialog(frame, "Error: " + e.getMessage(), "Excepcion", JOptionPane.WARNING_MESSAGE);
+                continuar = "si";
+            }
+        }while(continuar.equals("si"));
+        System.exit(0);
+    }
+}
